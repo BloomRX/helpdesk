2327323243ea8896e544ebd5cdb8ec4f39550213
BloomRX <lucasbarbano71@gmail.com>
2025-07-07 13:39:57 -0300

    005

--- Helpdesk.Api/Models/Usuario.cs
+++ Helpdesk.Api/Models/Usuario.cs
@@ -2,12 +2,6 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Helpdesk.Api.Models
 {
-    public enum TipoUsuario
-    {
-        Membro,
-        Admin
-    }
-
     public class Usuario
     {
         public int Id { get; set; }
