551fb58e452d60cf43c74193ad169c4cb050448c
BloomRX <lucasbarbano71@gmail.com>
2025-07-08 11:03:33 -0300

    009 - 3

--- Helpdesk.Api/obj/Debug/net9.0/Helpdesk.Api.AssemblyInfo.cs
+++ Helpdesk.Api/obj/Debug/net9.0/Helpdesk.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Helpdesk.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5f815953034192e764dab6c28d8dc65f675187b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69bb98e1ea44213da013280cdde6feeb6b321e16")]
 [assembly: System.Reflection.AssemblyProductAttribute("Helpdesk.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Helpdesk.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
