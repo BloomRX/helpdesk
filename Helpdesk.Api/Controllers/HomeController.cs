ad6722c745f29553410ad3104492351faefaa41d
BloomRX <lucasbarbano71@gmail.com>
2025-07-08 10:45:34 -0300

    008

--- Helpdesk.Api/Controllers/HomeController.cs
+++ Helpdesk.Api/Controllers/HomeController.cs
@@ -1,31 +1,45 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Helpdesk.Api.Models;
+using Helpdesk.Api.Data;
+using Microsoft.Extensions.Logging;
+using System.Linq;
 
-namespace Helpdesk.Api.Controllers;
-
-public class HomeController : Controller
+namespace Helpdesk.Api.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    public class HomeController : Controller
     {
-        _logger = logger;
-    }
+        private readonly ILogger<HomeController> _logger;
+        private readonly AppDbContext _context;
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+        public HomeController(ILogger<HomeController> logger, AppDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+        // Exibe as últimas 5 solicitações na home
+        public IActionResult Index()
+        {
+            var ultimasSolicitacoes = _context.Solicitacoes
+                .OrderByDescending(s => s.DataAbertura)
+                .Take(5)
+                .ToList();
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(ultimasSolicitacoes);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel {
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+            });
+        }
     }
 }
