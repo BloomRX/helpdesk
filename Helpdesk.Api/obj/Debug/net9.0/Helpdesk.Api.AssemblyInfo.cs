93b821a75a24f963adfc79164e33f63359fce3fb
BloomRX <lucasbarbano71@gmail.com>
2025-07-10 12:27:02 -0300

    Link Hover Blue

--- Helpdesk.Api/obj/Debug/net9.0/Helpdesk.Api.AssemblyInfo.cs
+++ Helpdesk.Api/obj/Debug/net9.0/Helpdesk.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Helpdesk.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce7b9f055f58755ab52fdf119dd74a11fa7c7001")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa4a377cb9ee582b8693ca1c8e5bfc8b24bc70fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Helpdesk.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Helpdesk.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
