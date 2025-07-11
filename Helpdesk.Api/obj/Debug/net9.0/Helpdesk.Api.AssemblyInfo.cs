a85a5c2c36eaf11f2b60853f1b78a55089be99d9
Exar2004 <141692448+Exar2004@users.noreply.github.com>
2025-07-11 11:08:14 -0300

    C

--- Helpdesk.Api/obj/Debug/net9.0/Helpdesk.Api.AssemblyInfo.cs
+++ Helpdesk.Api/obj/Debug/net9.0/Helpdesk.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Helpdesk.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf3321a426d8254f987e8320a0b76702147a5695")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b58f5bcc0bd4791e089cf9a973d10922c62a3ab4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Helpdesk.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Helpdesk.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
